de9b4e7a83ac17d94fb0345bf60ebee3de496b85
Przemek <przempore@gmail.com>
2023-12-03 21:52:22 +0100

    day3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,12 +1,23 @@
 use std::fs::read_to_string;
 
-fn part1(input: &str) -> u32 {
+fn find_numbers_and_symbols(
+    input: &str,
+) -> (
+    Vec<(u32, (usize, (usize, usize)))>,
+    Vec<(char, (usize, usize))>,
+) {
     let mut numbers = Vec::new();
     let mut signs = Vec::new();
     let mut current_number = String::new();
     let mut coord = (0, 0);
     for (line_idx, line) in input.lines().enumerate() {
         for (i, c) in line.chars().enumerate() {
+            if i == 0 && !current_number.is_empty() {
+                let parsed = current_number.parse::<u32>().unwrap();
+                numbers.push((parsed, (line_idx - 1, coord)));
+                current_number.clear();
+            }
+
             if c.is_digit(10) {
                 if current_number.is_empty() {
                     coord.0 = i;
@@ -18,7 +29,6 @@ fn part1(input: &str) -> u32 {
             } else {
                 if !current_number.is_empty() {
                     let parsed = current_number.parse::<u32>().unwrap();
-                    println!("num: {}, parsed: {}", current_number, parsed);
                     numbers.push((parsed, (line_idx, coord)));
                     current_number.clear();
                 }
@@ -29,8 +39,11 @@ fn part1(input: &str) -> u32 {
         }
     }
 
-    // println!("numbers: {:?}", numbers);
-    // println!("signs: {:?}", signs);
+    (numbers, signs)
+}
+
+fn part1(input: &str) -> u32 {
+    let (numbers, signs) = find_numbers_and_symbols(input);
 
     numbers
         .iter()
@@ -38,16 +51,11 @@ fn part1(input: &str) -> u32 {
             signs.iter().find_map(|(_, (l_idx, column_idx))| {
                 if line_idx == l_idx {
                     if column_idx + 1 == *begin || *column_idx == end + 1 {
-                        // same line
-                        print!("{} ", number);
                         return Some(*number);
                     }
                     return None;
                 } else if *line_idx == l_idx + 1 || line_idx + 1 == *l_idx {
-                    // sign is under the number
                     if column_idx + 1 >= *begin && *column_idx <= end + 1 {
-                        // same line
-                        print!("{} ", number);
                         return Some(*number);
                     }
                     return None;
@@ -56,6 +64,41 @@ fn part1(input: &str) -> u32 {
             })
         })
         .filter_map(|number| number)
+        .sum()
+}
+
+fn part2(input: &str) -> u32 {
+    let (numbers, signs) = find_numbers_and_symbols(input);
+
+    signs
+        .iter()
+        .map(|(sign, (line_idx, column_idx))| {
+            if *sign != '*' {
+                return None;
+            }
+
+            let mut pairs = Vec::new();
+            for (number, (l_idx, (begin, end))) in &numbers {
+                if line_idx == l_idx {
+                    if column_idx + 1 >= *begin && *column_idx <= end + 1 {
+                        pairs.push(*number);
+                    }
+                } else if *line_idx == l_idx + 1 || line_idx + 1 == *l_idx {
+                    if column_idx + 1 >= *begin && *column_idx <= end + 1 {
+                        pairs.push(*number);
+                    }
+                }
+                if pairs.len() == 2 {
+                    break;
+                }
+            }
+            if pairs.len() == 2 {
+                let power = pairs.iter().product::<u32>();
+                return Some(power);
+            }
+            None
+        })
+        .filter_map(|n| n)
         .sum()
 }
 
@@ -68,13 +111,13 @@ fn main() {
     let input = read_to_string("day3/src/input_part1.txt").expect("Error reading input_part1.txt");
     let result = part1(&input);
     println!("Part 1 result: {}", result);
-    //
-    // let input = read_to_string("day3/src/input_part1_example.txt")
-    //     .expect("Error reading input_part1_example.txt");
-    // let result = part2(&input);
-    // println!("Example part 2 result: {}", result);
-    //
-    // let input = read_to_string("day3/src/input_part1.txt").expect("Error reading input_part1.txt");
-    // let result = part2(&input);
-    // println!("Part 2 result: {}", result);
+
+    let input = read_to_string("day3/src/input_part1_example.txt")
+        .expect("Error reading input_part1_example.txt");
+    let result = part2(&input);
+    println!("Example part 2 result: {}", result);
+
+    let input = read_to_string("day3/src/input_part1.txt").expect("Error reading input_part1.txt");
+    let result = part2(&input);
+    println!("Part 2 result: {}", result);
 }
