35f568a6acd877772dd18c83b3ef42366ea4966f
Przemek <przempore@gmail.com>
2024-11-19 22:22:13 +0100

    update and add `cpp` shell

--- flake.nix
+++ flake.nix
@@ -1,21 +1,94 @@
 {
   inputs = {
-    naersk.url = "github:nix-community/naersk/master";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    # systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs = { nixpkgs.follows = "nixpkgs"; };
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy rust-analyzer ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-        };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, /* systems, */ ... } @ inputs:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      # forEachSystem = nixpkgs.lib.genAttrs (import systems);
+      forEachSystem = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+        devenv-test = self.devShells.${system}.default.config.test;
       });
+
+      # devShells = forEachSystem
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  # https://devenv.sh/reference/options/
+                  packages = [ pkgs.hello ];
+
+                  enterShell = ''
+                    hello
+                  '';
+
+                  processes.hello.exec = "hello";
+                }
+              ];
+            };
+
+            rust = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  # https://devenv.sh/reference/options/
+                  packages = [];
+
+                  languages.rust = {
+                    enable = true;
+                    channel = "nightly";
+                  };
+
+                  enterShell = ''
+                    cargo version
+                  '';
+
+                  # processes.hello.exec = "hello";
+                }
+              ];
+            };
+
+            cpp = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  # https://devenv.sh/reference/options/
+                  packages = [
+                    # pkgs.stdenv.cc.cc.lib
+                    pkgs.gcc
+                    pkgs.ninja
+                    pkgs.clang-tools
+                  ];
+
+                  enterShell = ''
+                    gcc --version
+                    echo "ninja version $(ninja --version)"
+                  '';
+                }
+              ];
+            };
+          });
+    };
 }
