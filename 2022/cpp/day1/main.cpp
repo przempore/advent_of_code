1ad760807fcc1815b4efe135e0f61aef8731e288
Przemek <przempore@gmail.com>
2024-11-22 23:49:03 +0100

    [2022] day 1 part 2

--- 2022/cpp/day1/main.cpp
+++ 2022/cpp/day1/main.cpp
@@ -1,6 +1,11 @@
 #include <fmt/core.h>
+#include <fmt/ranges.h>
 
+#include <algorithm>
+#include <array>
+#include <cstdint>
 #include <fstream>
+#include <numeric>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -12,7 +17,7 @@ void part_one() {
     uint64_t partial_sum{0};
     std::string line{};
     auto max = partial_sum;
-    while (std::getline(file, line)){
+    while (std::getline(file, line)) {
         // fmt::println("line: {}", line);
         if (line.empty() and partial_sum != 0) {
             sums.push_back(partial_sum);
@@ -31,15 +36,45 @@ void part_one() {
 
     sums.push_back(partial_sum);
 
-    // for (const auto& s : sums) {
-    //     fmt::println("s: {}", s);
-    // }
+    fmt::println("part one sum: {}", max);
+}
 
-    fmt::println("max: {}", max);
+void part_two() {
+    std::ifstream file("input.txt");
+
+    std::vector<uint64_t> sums{};
+    uint64_t partial_sum{0};
+    std::string line{};
+    std::array max = {partial_sum, partial_sum, partial_sum};
+    while (std::getline(file, line)) {
+        if (line.empty() and partial_sum != 0) {
+            sums.push_back(partial_sum);
+            partial_sum = 0;
+        }
+
+        std::istringstream iss(line);
+        auto tmp = partial_sum;
+        iss >> partial_sum;
+        partial_sum += tmp;
+    }
+    sums.push_back(partial_sum);
+
+    for (auto& s : sums) {
+        auto current_min = std::ranges::min_element(max.begin(), max.end());
+        if (*current_min < s) {
+            *current_min = s;
+        }
+    }
+
+    // fmt::print("{}", fmt::join(max, ", "));
+    // fmt::println("\n+++++++");
+
+    fmt::println("part two sum: {}", std::accumulate(max.begin(), max.end(), 0));
 }
 
 int main() {
     part_one();
+    part_two();
 
     return 0;
 }
